be355f35fa1ed9f99aa5784a130a54189c34ca5a
spartakgubarev <spartakgubarev@rambler.ru>
2022-07-27 10:59:30 +0300

    First BD

---
+++ Lesson01/Program.cs
@@ -0,0 +1,17 @@
+﻿// string result = string.Empty;
+
+// string[] marks = { "Отличник", "Ударник", "Троечник", "Двоечник" };
+// for (int i = 0; i < 10; i++)
+// {
+//     string fName = $"Имя_{i}";
+//     string lName = $"Имя_{i}";
+//     int id = i;
+//     string mark = marks[new Random().Next(marks.Length)];
+
+//     result += $"{id}\t{fName}\t{lName}\t{mark}\n";
+// }
+
+// //Console.Write($"{result}");
+// File.WriteAllText("db.csv", result, System.Text.Encoding.UTF32);
+
+
